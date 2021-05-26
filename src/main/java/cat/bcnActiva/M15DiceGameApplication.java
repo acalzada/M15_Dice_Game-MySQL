523c29c36e7a94fd3208040b41d2fc1b3476c2c6
merdaalcanto <merdaalcanto@hotmail.com>
2021-05-26 10:24:23 +0200

    initial commit

---
+++ src/main/java/cat/bcnActiva/M15DiceGameApplication.java
@@ -0,0 +1,13 @@
+package cat.bcnActiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M15DiceGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M15DiceGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/bcnActiva/M15DiceGameApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.bcnActiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M15DiceGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
